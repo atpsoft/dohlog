6681d24a39c313c8096f1e08939975de443df773
Makani Mason <gitauthor@pqmland.com>
2012-10-01 17:21:22 -0600

    add an else acceptor to the FilterAcceptor

--- lib/doh/log/filter_acceptor.rb
+++ lib/doh/log/filter_acceptor.rb
@@ -1,14 +1,17 @@
 module DohLog
 
 class FilterAcceptor
-  def initialize(acceptor, &pred)
+  def initialize(acceptor, else_acceptor = nil, &pred)
     @acceptor = acceptor
+    @else_acceptor = else_acceptor
     @pred = pred
   end
 
   def add(event)
     if @pred.call(event)
       @acceptor.add(event)
+    elsif @else_acceptor
+      @else_acceptor.add(event)
     end
   end
 
